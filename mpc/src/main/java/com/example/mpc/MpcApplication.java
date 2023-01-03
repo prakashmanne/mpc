06a200d0aea49d8402fde9e71844c73d5aea50ac
prakashmanne <manneprakash0@gmail.com>
2023-01-03 12:32:20 -0600

    new project

---
+++ mpc/src/main/java/com/example/mpc/MpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpcApplication.class, args);
+	}
+
+}
---
+++ mpc/src/test/java/com/example/mpc/MpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
